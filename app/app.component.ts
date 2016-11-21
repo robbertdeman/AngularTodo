ac4b4c231ec9f4d2e6b39f3f7275d36786e7206f
robbertdeman <robbertdeman@hotmail.com>
2016-11-21 15:07:23 +0100

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,47 @@
+//hier moet je de modules importeren
+
+export class TodoItem {
+    constructor(public text: string, public completed: boolean) {
+
+    }
+}
+
+@Component({
+    selector: 'my-app',
+})
+@View({
+
+})
+
+export class AppComponent {
+    todos: Array<TodoItem>;
+
+    constructor() {
+        this.todos = new Array<TodoItem>();
+    }
+
+    //functie maken die de item checked
+    setCompleted() {
+
+    }
+
+    //functie maken die de item removed
+    removeTodo() {
+
+    }
+
+    //functie maken die de kijkt of er op enter gedrukt is
+    doneTyping() {
+
+    }
+
+    //item aanmaken
+    addTodo() {
+
+    }
+
+    //functie die alle items checked
+    completeAll() {
+
+    }
+}
